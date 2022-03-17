2d93f0633196f5a1a03543ef897a0677f4ed464d
HMZKA <hmzakh65@gmail.com>
2022-03-18 01:24:10 +0200

    register

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_4/login_screen.dart';
+import 'package:flutter_application_4/register_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,82 +12,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          leading: Icon(Icons.menu),
-          title: Text('Login'),
-          backgroundColor: Colors.white,
-          foregroundColor: Colors.black,
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: Center(
-            child: SingleChildScrollView(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Login',
-                    style: TextStyle(fontSize: 35),
-                  ),
-                  SizedBox(
-                    height: 40,
-                  ),
-                  TextFormField(
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(
-                        prefixIcon: Icon(Icons.email),
-                        labelText: 'Email',
-                        border: OutlineInputBorder()),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  TextFormField(
-                    obscureText: true,
-                    keyboardType: TextInputType.visiblePassword,
-                    decoration: InputDecoration(
-                        prefixIcon: Icon(CupertinoIcons.padlock),
-                        suffixIcon: IconButton(
-                          icon: Icon(
-                            Icons.remove_red_eye_outlined,
-                          ),
-                          onPressed: () {},
-                        ),
-                        labelText: 'Password',
-                        border: OutlineInputBorder()),
-                  ),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  Container(
-                    color: Colors.blue,
-                    child: MaterialButton(
-                      onPressed: () {},
-                      child: Center(
-                          child: Text(
-                        'Login',
-                        style: TextStyle(color: Colors.white),
-                      )),
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text('Dont\'t have account?'),
-                      TextButton(onPressed: () {}, child: Text('Register now'))
-                    ],
-                  )
-                ],
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: LoginScreen());
   }
 }
---
+++ lib/register_screen.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+
+class RegisterScreen extends StatefulWidget {
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  var formkey = GlobalKey<FormState>();
+  bool scure = true;
+  Widget icon = Icon(Icons.visibility_off);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        title: Text('Register'),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: SingleChildScrollView(
+          child: Form(
+            key: formkey,
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 40,
+                ),
+                TextFormField(
+                  onFieldSubmitted: (value) {
+                    print(value);
+                  },
+                  onChanged: (value) {
+                    print(value);
+                  },
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'This must not be empty';
+                    }
+                  },
+                  decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.person),
+                      labelText: 'Username',
+                      border: OutlineInputBorder()),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                TextFormField(
+                  validator: (value) {
+                    if (value!.isEmpty || !value.contains('@')) {
+                      return 'This must not be empty';
+                    }
+                  },
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.email),
+                      labelText: 'email',
+                      border: OutlineInputBorder()),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                TextFormField(
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'This must not be empty';
+                    }
+                  },
+                  keyboardType: TextInputType.phone,
+                  decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.phone),
+                      labelText: 'Phone',
+                      border: OutlineInputBorder()),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                TextFormField(
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'This must not be empty';
+                    }
+                  },
+                  obscureText: scure,
+                  keyboardType: TextInputType.visiblePassword,
+                  decoration: InputDecoration(
+                      suffixIcon: IconButton(
+                        icon: icon,
+                        onPressed: () {
+                          setState(() {
+                            if (scure == true) {
+                              scure = false;
+                              icon = Icon(Icons.remove_red_eye);
+                            } else {
+                              scure = true;
+                              icon = Icon(Icons.visibility_off);
+                            }
+                          });
+                        },
+                      ),
+                      prefixIcon: Icon(Icons.lock),
+                      labelText: 'password',
+                      border: OutlineInputBorder()),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                TextButton(
+                    onPressed: () {
+                      if (formkey.currentState!.validate()) {
+                        print('false');
+                      }
+                    },
+                    child: Container(
+                        color: Colors.blue,
+                        width: double.infinity,
+                        height: 50,
+                        child: Center(
+                            child: Text(
+                          'Register',
+                          style: TextStyle(color: Colors.white),
+                        )))),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
